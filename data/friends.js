179cb6e47b7c7c482720007fceb851b899695ccc
Wayne Wu <mrwu42@gmail.com>
2020-03-26 11:49:59 -0400

    unresolved CORS error

--- data/friends.js
+++ data/friends.js
@@ -1,9 +1,9 @@
-var friends = [{ "name": "Thanos", "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] },
-                { "name": "Jacob Deming", "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] },
-                { "name": "Jeremiah Scanlon", "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] },
-                { "name": "Louis T. Delia", "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] },
-                { "name": "Lou Ritter", "photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] },
-                { "name": "Jordan Biason", "photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+var friends = [{ "name": "Thanos", "photo": "https://vignette.wikia.nocookie.net/marveldatabase/images/c/cb/Infinity_Vol_1_4_Generals_Variant_Textless.jpg/revision/latest?cb=20130628193007", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] },
+                { "name": "Ebony Maw", "photo": "https://i.ytimg.com/vi/ri1UBxhOzWc/maxresdefault.jpg", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] },
+                { "name": "Corvus Glaive", "photo": "https://vignette.wikia.nocookie.net/marveldatabase/images/1/19/Corvus_Glaive_%28Earth-616%29_from_New_Avengers_Vol_3_8_001.jpg/revision/latest?cb=20160128184448", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] },
+                { "name": "Proxima Midnight", "photo": "https://boundingintocomics.com/files/2018/03/Proxima-Midnight.png", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] },
+                { "name": "Dr Doom", "photo": "https://vignette.wikia.nocookie.net/marveldatabase/images/f/f9/Victor_von_Doom_%28Earth-616%29_from_Thor_Vol_1_600.jpg/revision/latest?cb=20130528050125", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] },
+                { "name": "Darkseid", "photo": "https://am23.mediaite.com/tms/cnt/uploads/2019/08/The-internet-is-trying-to-decide-is-darkseid-is-better-or-thanos.jpg", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
 
 
 module.exports = friends;
